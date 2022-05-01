4a3352d00d7a870740a3e252c1c363ea1b969066
DmitryAbo <AbornevDmitry@gmail.com>
2022-05-01 23:49:51 +0300

    use junit4

---
+++ src/test/java/ru/netology/service/CashbackHackerTest.java
@@ -0,0 +1,65 @@
+package ru.netology.service;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class CashbackHackerTest {
+
+    @Test
+    public void shouldLowBorderAmount() {
+        CashbackHacker service = new CashbackHacker();
+        int expected = 1000;
+        int actual = service.remain(0);
+        assertEquals(actual, expected);
+    }
+
+    @Test
+    public void shouldLowBorderGreaterAmount() {
+        CashbackHacker service = new CashbackHacker();
+        int expected = 999;
+        int actual = service.remain(1);
+        assertEquals(actual, expected);
+    }
+
+    @Test
+    public void shouldIntermediateAmount() {
+        CashbackHacker service = new CashbackHacker();
+        int expected = 500;
+        int actual = service.remain(500);
+        assertEquals(actual, expected);
+    }
+
+    @Test
+    public void shouldHighBorderLessAmount() {
+        CashbackHacker service = new CashbackHacker();
+        int expected = 1;
+        int actual = service.remain(999);
+        assertEquals(actual, expected);
+    }
+
+    @Test
+    public void shouldHighBorderAmount() {
+        CashbackHacker service = new CashbackHacker();
+        int expected = 1000;
+        int actual = service.remain(1000);
+        assertEquals(actual, expected);
+    }
+
+    @Test
+    public void shouldHighBorderGreaterAmount() {
+        CashbackHacker service = new CashbackHacker();
+        int expected = 999;
+        int actual = service.remain(1001);
+        assertEquals(actual, expected);
+    }
+
+    @Test
+    public void shouldHighIntermediateAmount() {
+        CashbackHacker service = new CashbackHacker();
+        int expected = 350;
+        int actual = service.remain(4650);
+        assertEquals(actual, expected);
+    }
+
+}
