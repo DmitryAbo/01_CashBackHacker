5e497c4048f79950da2709206c43384a93f51c7f
DmitryAbo <AbornevDmitry@gmail.com>
2022-05-01 22:26:32 +0300

    use testng platform

---
+++ src/test/java/ru/netology/service/CashbackHackerTest.java
@@ -0,0 +1,74 @@
+package ru.netology.service;
+
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.*;
+
+public class CashbackHackerTest {
+
+    @Test
+    public void shouldLowBorderLessAmount() {
+        CashbackHacker service = new CashbackHacker();
+        int actual = service.remain(-1);
+        expectThrows(RuntimeException.class, () -> {
+            service.remain(-1);
+        });
+    }
+
+    @Test
+    public void shouldLowBorderAmount() {
+        CashbackHacker service = new CashbackHacker();
+        int expected = 1000;
+        int actual = service.remain(0);
+        assertEquals(actual, expected);
+    }
+
+    @Test
+    public void shouldLowBorderGreaterAmount() {
+        CashbackHacker service = new CashbackHacker();
+        int expected = 999;
+        int actual = service.remain(1);
+        assertEquals(actual, expected);
+    }
+
+    @Test
+    public void shouldIntermediateAmount() {
+        CashbackHacker service = new CashbackHacker();
+        int expected = 500;
+        int actual = service.remain(500);
+        assertEquals(actual, expected);
+    }
+
+    @Test
+    public void shouldHighBorderLessAmount() {
+        CashbackHacker service = new CashbackHacker();
+        int expected = 1;
+        int actual = service.remain(999);
+        assertEquals(actual, expected);
+    }
+
+    @Test
+    public void shouldHighBorderAmount() {
+        CashbackHacker service = new CashbackHacker();
+        int expected = 1000;
+        int actual = service.remain(1000);
+        assertEquals(actual, expected);
+    }
+
+    @Test
+    public void shouldHighBorderGreaterAmount() {
+        CashbackHacker service = new CashbackHacker();
+        int expected = 999;
+        int actual = service.remain(1001);
+        assertEquals(actual, expected);
+    }
+
+    @Test
+    public void shouldHighIntermediateAmount() {
+        CashbackHacker service = new CashbackHacker();
+        int expected = 350;
+        int actual = service.remain(4650);
+        assertEquals(actual, expected);
+    }
+
+}
